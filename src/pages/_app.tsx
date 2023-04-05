22510979fa11280a8e1803231cac72fec7a3b6cc
Flávio Junior <flaviopds.jr@gmail.com>
2023-04-05 18:06:12 -0300

    feat: add google ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { initGA, logPageView } from "~/utils/analytics";
-import ReactGA from "react-ga";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    ReactGA.initialize("G-BJBVE01M5K");
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
